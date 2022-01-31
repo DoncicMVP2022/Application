9f2070d1c6b72a89e7d49839d29dd33420547018
Zuev Daniil <daniilzuev216@gmail.com>
2022-01-31 11:36:17 +0300

    added database conn

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,6 +80,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -90,7 +92,10 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
